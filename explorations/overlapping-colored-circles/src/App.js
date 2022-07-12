1504045f985bef04c4ef3136e50a8a6eea29cba6
dom void <doe@onet.pl>
2022-07-12 15:37:22 +0200

    Feat set of circles and title

--- explorations/overlapping-colored-circles/src/App.js
+++ explorations/overlapping-colored-circles/src/App.js
@@ -1,19 +1,20 @@
 import Circle from './components/circle';
+import Title from './components/title';
 import './App.css';
 
 function App() {
-  
+
   return (
     <div className="App">
-      <Circle color="#EC4E3D"/>
-      <Circle color="#2E7CF6"/>
-      <Circle color="#F8CD46"/>
-      <Circle color="red"/>
-      <Circle color="green"/>
-      <Circle color="blue"/>
-      <Circle color="gray"/>
-      <Circle color="black"/>
-      <Circle color="whitesmoke"/>
+      <Title />
+      <Circle color="#EC4E3D" />
+      <Circle color="#2E7CF6" />
+      <Circle color="#F8CD46" />
+      <Circle color="red" />
+      <Circle color="green" />
+      <Circle color="blue" />
+      <Circle color="gray" />
+      <Circle color="whitesmoke" />
     </div>
   );
 }

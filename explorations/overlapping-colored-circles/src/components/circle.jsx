0aae0e2cbc09a1a52e1250ef02c17ee3b1170703
dom void <doe@onet.pl>
2022-07-12 08:51:58 +0200

    Produce one not properly moving div

--- explorations/overlapping-colored-circles/src/components/circle.jsx
+++ explorations/overlapping-colored-circles/src/components/circle.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './circle.css'
 
 const Circle = () => {
@@ -9,24 +9,38 @@ const Circle = () => {
         left: 0,
     };
 
-    const [position, setPosition] = useState({x: 0, y: 0});
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [direction, setDirection] = useState({ x: 1, y: 1 });
     const [style, setStyle] = useState(initStyle);
 
-/* 
-    const { innerWidth, innerHeight } = window;
-    const calculatePosition = (x, y) => {
-
-    };
-*/
+    let innerWidth, innerHeight;
+    const calculatePosition = useCallback((x, y) => {
+        if (y === innerWidth - 0.1 * innerWidth || y === -1) {
+            setDirection((direction) => ({ ...direction, y: -1 * direction.y }))
+        }
+        if (x === innerHeight - 0.1 * innerHeight || x === -1) {
+            setDirection((direction) => ({ ...direction, x: -1 * direction.x }))
+        }
+        return {
+            x: x + direction.x,
+            y: y + direction.y,
+        };
+    }, [direction, innerHeight, innerWidth]);
 
     useEffect(() => {
+        ({ innerWidth, innerHeight } = window);
+    }, []);
+
+    useEffect(() => {
+        console.log(innerWidth, innerHeight);
         let intervalId = setInterval(() => {
-            setPosition((position) => ({...position, x: ++position.x, y: ++position.y}))
+            const { x, y } = calculatePosition(position.x, position.y);
+            setPosition((position) => ({ ...position, x, y }));
             setStyle((style) => ({ ...style, top: `${position.y}px`, left: `${position.y}px` }));
             console.log(style);
-        }, 100);
+        }, 30);
         return () => clearInterval(intervalId);
-    }, [position, style]);
+    }, [calculatePosition, position, style, innerHeight, innerWidth]);
 
     return (
         <div className='circle' style={style}></div>

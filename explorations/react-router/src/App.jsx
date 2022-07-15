b9554fd60222b1ec63fe8ab80902086ad9f04544
dom void <doe@onet.pl>
2022-07-15 11:13:06 +0200

    Init react-router exploration

---
+++ explorations/react-router/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './compnents/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>It works!</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ explorations/react-router/src/compnents/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h3>This is a header</h3>
+        </header>
+    );
+}
+
+export default Header;
